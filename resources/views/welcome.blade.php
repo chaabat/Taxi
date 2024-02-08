15ce8ede64a92446221220fb697a175565aa86a3
chaabat <chaabat.02@gmail.com>
2024-02-08 16:15:20 +0100

    logine and registre done avec les role

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
                 </button>
             </div>
 			<div class="flex flex-col items-center justify-center bg-[#ffb703] p-12"> 
-                <img src="{{asset('photos/logo.png')}}" class="h-10 " alt="">
+                <img src="{{asset('photos/logoo.png')}}" class="h-[100px] " alt="">
 
 				<div class="mt-3 md:flex md:items-center md:-mx-2">
 					<a href="{{ route('registrePassager') }}" class="text-[#ffb703] bg-black focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center">
